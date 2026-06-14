c365cac8ee9afb4cf670f43cc0eb8b98f3e406de
jelena3160 <jelena3160@gmail.com>
2026-06-14 12:28:45 +0200

    added docker and swagger to movie api

--- Services/Movie.API/Program.cs
+++ Services/Movie.API/Program.cs
@@ -12,17 +12,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IMovieContext, MovieContext>();
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
