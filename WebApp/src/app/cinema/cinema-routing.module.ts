6f1ae0cc9869d97608c397c582ab0da6a0b70162
Marijameme <marijanacupovic@gmail.com>
2026-08-30 14:51:06 +0200

    Added form for editing and adding a cinema

--- WebApp/src/app/cinema/cinema-routing.module.ts
+++ WebApp/src/app/cinema/cinema-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CinemaListComponent } from './components/cinema-list/cinema-list.component';
 import { CinemaFormComponent } from './components/cinema-form/cinema-form.component';
-import { CinemaDetailComponent } from './components/cinema-detail/cinema-detail.component';
-import { HallSeatsComponent } from './components/hall-seats/hall-seats.component';
+
 
 
 const routes: Routes = [
   { path: '', component: CinemaListComponent },
-];
+  { path: 'new', component: CinemaFormComponent },
+  { path: ':id/edit', component: CinemaFormComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
