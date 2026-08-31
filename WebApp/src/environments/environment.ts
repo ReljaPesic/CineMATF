d1b99ef705a81db116b39b84014aaffb35ced623
Marijameme <marijanacupovic@gmail.com>
2026-08-31 15:04:24 +0200

    Added models and services for Screening.API

--- WebApp/src/environments/environment.ts
+++ WebApp/src/environments/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: true,
   api: {
     movies: 'http://localhost:5011/api/v1',
-    cinema: 'http://localhost:5000/api/v1'
+    cinema: 'http://localhost:5000/api/v1',
+    screening: 'http://localhost:5138/api/v1'
   },
 };
